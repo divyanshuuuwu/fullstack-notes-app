941856a1c701139a1a40c4426a853690eabad0b6
divyanshu <divyanshusah44@gmail.com>
2026-07-27 21:25:09 +0530

    getting all the data .get

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,9 +11,19 @@ app.post("/create", async(req , res)=>{
         description: data.description,
         date: data.date
     })
-    res.status(201).json({
+    return res.status(201).json({
         message:"completed sucessfully"
     })
+
+})
+
+app.get("/show", async(req , res)=>{
+    const data = await noteModel.find()
+        return res.status(200).json({
+            message: "data fetched successfully",
+            notes : data
+    })
+
 
 })
 
